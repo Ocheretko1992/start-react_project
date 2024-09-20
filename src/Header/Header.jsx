bbb7028835a491d31f8383d3e24b4bdf3f18a9f5
Ocheretko Oleh <you@example.com>
2024-09-20 23:14:00 +0300

    sefsfds

--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -1,14 +1,10 @@
 import css from "./Header.module.css";
+import { FcGlobe } from "react-icons/fc";
 
 const Header = () => {
   return (
     <header className={css.header}>
-      <img
-        src="https://img.freepik.com/free-vector/detailed-welder-logo-template_23-2149040500.jpg?t=st=1726710083~exp=1726713683~hmac=a26986b5ebdeb5bd4b350d7117f3d6834573bfbc4b47eb837edd42a70508b722&"
-        width={40}
-        height={40}
-        alt="welders"
-      />
+      <FcGlobe className={css.earth} />
       <ul className={css.style}>
         <li>Home</li>
         <li>Registration</li>
