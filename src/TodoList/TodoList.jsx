af7144d8635ebc1de930fb2b0567e27a59e4eb32
Ocheretko Oleh <you@example.com>
2024-09-20 16:11:01 +0300

    fdgdfvb

--- src/TodoList/TodoList.jsx
+++ src/TodoList/TodoList.jsx
@@ -1,9 +1,13 @@
+import s from "./TodoList.module.css";
+
 const TodoList = ({ colors }) => {
   return (
     <div>
-      <ul>
+      <ul className={s.list}>
         {colors.map(({ color, id }) => (
-          <li key={id}>{color}</li>
+          <li key={id} className={s.liList}>
+            {color},
+          </li>
         ))}
       </ul>
     </div>
