cb5d5beb4c0df59a0da90eeb5f1123bce31f0e09
Ocheretko Oleh <you@example.com>
2024-09-20 16:15:14 +0300

    new create

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -15,12 +15,6 @@ const App = () => {
       {age >= 18 ? (
         <Permissions>
           <h1>Hello Oleh</h1>
-          <p>
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Pariatur
-            iusto, laudantium, et provident perspiciatis nesciunt inventore,
-            odit placeat praesentium autem natus animi id doloribus minus eos.
-            Deleniti ab corrupti sunt.
-          </p>
           <button type="button" style={{ color: "red", background: "green" }}>
             nice
           </button>
